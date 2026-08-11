e04d8fcbe72786a572f78cde7f0437fa03a63240
Andrés Abundis <andresabundis@gmail.com>
2026-08-10 20:49:51 -0600

    First review fixes

--- week1-tdd/1_5_api_testing/github_client.py
+++ week1-tdd/1_5_api_testing/github_client.py
@@ -1,6 +1,16 @@
 import httpx
 
-class ValidationFailed(Exception):
+class UserNotFoundError(Exception):
+    def __init__(self, message, partial_data):
+        super().__init__(message)
+        self.partial_data = partial_data
+
+class GitHubServerError(Exception):
+    def __init__(self, message, partial_data):
+        super().__init__(message)
+        self.partial_data = partial_data
+
+class GenericError(Exception):
     def __init__(self, message, partial_data):
         super().__init__(message)
         self.partial_data = partial_data
@@ -8,29 +18,25 @@ class ValidationFailed(Exception):
 def get_github_user(username: str):
     url = f"https://api.github.com/users/{username}"
     response = httpx.get(url)
-    try:
-        if response.status_code == 404:
-            raise ValidationFailed(f"Key error", {"status": response.status_code, "text": f"User '{username}' not found."})
-        if response.status_code >= 500:
-            raise ValidationFailed("Server error", {"status": response.status_code, "text": response.text})
-        else:
-            print(f"API Error: {response.status_code}: {response.text}")
-    except httpx.HTTPStatusError as e:
-        raise ValidationFailed(f"Failed to fetch user data", {"status": e.response.status_code, "text": e.response.text})
-    return response
+
+    if response.status_code == 404:
+        raise UserNotFoundError(f"User '{username}' not found.", {"status": response.status_code, "text": f"User '{username}' not found."})
+    if response.status_code >= 500:
+        raise GitHubServerError("Server error", {"status": response.status_code, "text": response.text})
+    if response.status_code < 200 or response.status_code >= 300:
+        raise GenericError("Generic error", {"status": response.status_code, "text": response.text})
+
+    return response.json()
 
 def get_user_public_repos(username: str):
     url = f"https://api.github.com/users/{username}/repos"
     response = httpx.get(url)
-    try:
-        if response.status_code == 404:
-            raise ValidationFailed(f"Key error", {"status": response.status_code, "text": f"User '{username}' not found."})
-        if response.status_code >= 500:
-            raise ValidationFailed("Server error", {"status": response.status_code, "text": response.text})
-        else:
-            print(f"API Error: {response.status_code}: {response.text}")
-    except httpx.HTTPStatusError as e:
-        raise ValidationFailed(f"Failed to fetch public repos", {"status": e.response.status_code, "text": e.response.text})
-    return response
+    
+    if response.status_code == 404:
+        raise UserNotFoundError(f"Key error", {"status": response.status_code, "text": f"User '{username}' not found."})
+    if response.status_code >= 500:
+        raise GitHubServerError("Server error", {"status": response.status_code, "text": response.text})
+    if response.status_code < 200 or response.status_code >= 300:
+        raise GenericError("Generic error", {"status": response.status_code, "text": response.text})
 
-# main
+    return response.json()
--- week1-tdd/1_5_api_testing/test_github_client.py
+++ week1-tdd/1_5_api_testing/test_github_client.py
@@ -5,8 +5,12 @@ import github_client as gh_client
 @pytest.mark.integration
 def test_gh_get_user_real():
     response = gh_client.get_github_user("octocat")
-    assert response.status_code == 200
-    assert response.json()["login"] == "octocat"
+    assert response["login"] == "octocat"
+
+@pytest.mark.integration
+def test_gh_get_user_repos_real():
+    response = gh_client.get_user_public_repos("octocat")
+    assert isinstance(response, list)
 
 # --- mocked test (no network) ---
 @respx.mock
@@ -14,7 +18,7 @@ def test_gh_user_handle_404():
     respx.get("https://api.github.com/users/na").mock(
         return_value=httpx.Response(404, json={"text": "User not found"})
     )
-    with pytest.raises(gh_client.ValidationFailed) as exc_info:
+    with pytest.raises(gh_client.UserNotFoundError) as exc_info:
         gh_client.get_github_user("na")
     assert exc_info.value.partial_data["status"] == 404
 
@@ -23,9 +27,18 @@ def test_gh_user_handle_500():
     respx.get("https://api.github.com/users/octocat").mock(
         return_value=httpx.Response(500, json={"text": "Server not available"})
     )
-    with pytest.raises(gh_client.ValidationFailed) as exc_info:
+    with pytest.raises(gh_client.GitHubServerError) as exc_info:
         gh_client.get_github_user("octocat")
     assert exc_info.value.partial_data["status"] == 500
+
+@respx.mock
+def test_gh_user_handle_400():
+    respx.get("https://api.github.com/users/octocat").mock(
+        return_value=httpx.Response(400, json={"text": "Bad request"})
+    )
+    with pytest.raises(gh_client.GenericError) as exc_info:
+        gh_client.get_github_user("octocat")
+    assert exc_info.value.partial_data["status"] == 400
 
 @respx.mock
 def test_gh_user_found():
@@ -33,15 +46,15 @@ def test_gh_user_found():
         return_value=httpx.Response(200, json={"login": "octocat"})
     )
     response = gh_client.get_github_user("octocat")
-    assert response.status_code == 200
-    assert response.json()["login"] == "octocat"
+
+    assert response["login"] == "octocat"
 
 @respx.mock
 def test_gh_user_repos_handle_404():
     respx.get("https://api.github.com/users/na/repos").mock(
         return_value=httpx.Response(404, json={"text": "User not found"})
     )
-    with pytest.raises(gh_client.ValidationFailed) as exc_info:
+    with pytest.raises(gh_client.UserNotFoundError) as exc_info:
         gh_client.get_user_public_repos("na")
     assert exc_info.value.partial_data["status"] == 404
 
@@ -50,15 +63,24 @@ def test_gh_user_repos_handle_500():
     respx.get("https://api.github.com/users/octocat/repos").mock(
         return_value=httpx.Response(500, json={"text": "Server not available"})
     )
-    with pytest.raises(gh_client.ValidationFailed) as exc_info:
+    with pytest.raises(gh_client.GitHubServerError) as exc_info:
         gh_client.get_user_public_repos("octocat")
     assert exc_info.value.partial_data["status"] == 500
 
 @respx.mock
+def test_gh_user_repos_handle_400():
+    respx.get("https://api.github.com/users/octocat/repos").mock(
+        return_value=httpx.Response(400, json={"text": "Bad request"})
+    )
+    with pytest.raises(gh_client.GenericError) as exc_info:
+        gh_client.get_user_public_repos("octocat")
+    assert exc_info.value.partial_data["status"] == 400
+
+@respx.mock
 def test_gh_user_repos_found():
     respx.get("https://api.github.com/users/octocat/repos").mock(
-        return_value=httpx.Response(200, json={"login": "octocat"})
+        return_value=httpx.Response(200, json=[{"name": "Hello-World"}, {"name": "Spoon-Knife"}])
     )
     response = gh_client.get_user_public_repos("octocat")
-    assert response.status_code == 200
-    assert response.json()["login"] == "octocat"
+
+    assert isinstance(response, list)
